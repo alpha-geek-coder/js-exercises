2e86320b16d04e133e69892f48587bd11592480c
alpha-geek-coder <76915453+alpha-geek-coder@users.noreply.github.com>
2025-05-30 16:02:03 -0400

    Introduce UI to play game
    Screen includes 3 buttons, one for each selection. User selects option
    by clicking button (Rock, Paper, Scissors). The result of a round
    is displayed below the buttons. Player to reach 5 points first wins.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-// const prompt = require("prompt-sync")({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
 const win_conditions = {
   ROCK: "SCISSORS",
   PAPER: "ROCK",
@@ -13,69 +7,61 @@ const win_conditions = {
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice() {
-    let random_idx = Math.floor(Math.random() * 3);
-    return choices[random_idx];
+  let random_idx = Math.floor(Math.random() * 3);
+  return choices[random_idx];
 }
 
 function getHumanChoice() {
+  while (true) {
+    let choice = prompt("Enter your choice (Rock or Paper or Scissors) :");
 
-    while (true) {
-        let choice = prompt("Enter your choice (Rock or Paper or Scissors) :");
-        
-        if (choice == null || choice.length == 0) {
-            console.log("Empty selection, retry");
-            continue;
-        }
-        choice = choice.toUpperCase();
-        if (choice == "ROCK" || choice == "PAPER" || choice == "SCISSORS") {
-            return choice;
-        } else {
-            console.log("Invalid entry!!! Try again...")
-        }
-            
+    if (choice == null || choice.length == 0) {
+      console.log("Empty selection, retry");
+      continue;
     }
-    
+    choice = choice.toUpperCase();
+    if (choice == "ROCK" || choice == "PAPER" || choice == "SCISSORS") {
+      return choice;
+    } else {
+      console.log("Invalid entry!!! Try again...");
+    }
+  }
 }
 
 function playRound(human_choice, computer_choice) {
-
-    console.log(`Your choice is ${human_choice}`);
-    console.log(`Computer's choice is ${computer_choice}`);
-    if (human_choice == computer_choice) {
-        console.log("It's a TIE!");
-        tie_score++;
-        return;
-    }
-    if (win_conditions[human_choice] === computer_choice) {
-        console.log(`You WIN! ${human_choice} beats ${computer_choice}`);
-        human_score++;
-    } else {
-        console.log(`Computer WINS! ${computer_choice} beats ${human_choice}`);
-        computer_score++;
-    }
-
-}
-
-function playGame() {
-
-  for (let round = 1; round <= 5; round++) {
-    console.log("Round " + round);   
-    playRound(getHumanChoice(), getComputerChoice());
+  console.log(`Your choice is ${human_choice}`);
+  console.log(`Computer's choice is ${computer_choice}`);
+  if (human_choice == computer_choice) {
+    return "It's a TIE!";
   }
-    console.log(`Final Score: Human vs Computer ${human_score} - ${computer_score} - ${tie_score}`);
-    if (human_score > computer_score) {
-        console.log("Hooray! YOU WIN!!!");
-    } else if (computer_score > human_score) {
-        console.log("YOU LOSE!")
-    } else {
-        console.log("IT'S A TIE!!!")
-    }
+  if (win_conditions[human_choice] === computer_choice) {
+    human_score++;
+    return `You WIN! ${human_choice} beats ${computer_choice}`;
+  } else {
+    computer_score++;
+    return `Computer WINS! ${computer_choice} beats ${human_choice}`;
+  }
 }
-let human_score = computer_score = tie_score = 0;
+
+let human_score = (computer_score = 0);
 
 console.log("Let's play Rock, Paper, Scissors");
 
-playGame();
-
-
-
+const button = document.querySelectorAll("button");
+const div = document.querySelector("div");
+button.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const selection = e.target.id;
+    const result = playRound(selection.toUpperCase(), getComputerChoice());
+    console.log(result);
+    if (human_score === 5) {
+      div.textContent = `Final Score: You vs Computer ${human_score} - ${computer_score} YOU WIN!!!`;
+      human_score = computer_score = 0;
+    } else if (computer_score === 5) {
+      div.textContent = `Final Score: You vs Computer ${human_score} - ${computer_score} Computer WINS!!!`;
+      human_score = computer_score = 0;
+    } else {
+      div.textContent = `${result} Score: You vs Computer ${human_score} - ${computer_score}`;
+    }
+  });
+});
